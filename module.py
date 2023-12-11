30eee8d97880780f8c2afb1396a44ff4ac632d36
Christopher Swenson <swenson@swenson.io>
2023-12-10 16:17:45 -0800

    Can completely parse sections for large binary

--- module.py
+++ module.py
@@ -1,8 +1,12 @@
 from collections import namedtuple
 from enum import IntEnum
-from typing import Callable, Tuple
+from typing import Callable, Tuple, BinaryIO
 import sys
 import struct
+import io
+
+# necessary for large function parsing
+sys.setrecursionlimit(100000)
 
 Section = namedtuple("Section", ["section_id", "contents"])
 CustomSection = namedtuple("CustomSection", ["name", "bytes"])
@@ -69,7 +73,7 @@ def read_module(f: bytes):
     while current < len(f):
         section_id = f[current]
         current += 1
-        section_size, length = read_u32(f[current:])
+        section_size, length = read_u32_bytes(f[current:])
         current += length
         contents = f[current : current + section_size]
         current += section_size
@@ -100,9 +104,9 @@ def read_module(f: bytes):
         elif section_id == ELEMENT_SECTION_ID:
             section = parse_element_section(contents)
             print(f"Element section, {len(section.elemsec)} elements")
-        # elif section_id == CODE_SECTION_ID:
-        #     section = parse_code_section(contents)
-        #     print(f"Code section, {len(section.code)} entries")
+        elif section_id == CODE_SECTION_ID:
+            section = parse_code_section(contents)
+            print(f"Code section, {len(section.code)} entries")
         elif section_id == DATA_SECTION_ID:
             section = parse_data_section(contents)
             print(f"Data section, {len(section.seg)} entries")
@@ -113,310 +117,372 @@ def read_module(f: bytes):
 
 
 def parse_data_section(raw: bytes) -> DataSection:
-    data, _ = read_vector(raw, decoder=read_data)
+    r = io.BytesIO(raw)
+    data = read_vector(r, decoder=read_data)
     return DataSection(data)
 
 
-def read_data(raw: bytes) -> (Data, int):
-    x, l1 = read_u32(raw)
-    x = MemIdx(x)
-    e, l2 = read_expr(raw[l1:])
-    b, l3 = read_vector_bytes(raw[l1 + l2 :])
-    return Data(x, e, b), l1 + l2 + l3
-
-
 def parse_code_section(raw: bytes) -> CodeSection:
-    code, _ = read_vector(raw, decoder=read_code)
+    r = io.BytesIO(raw)
+    code = read_vector(r, decoder=read_code)
     return CodeSection(code)
 
 
-def read_code(raw: bytes) -> (Code, int):
-    size, l1 = read_u32(raw)
-    # print(f"code size {size}")
-    # code, l2 = read_func(raw[l1:])
-    return Code(size, None), l1 + size
-
-
-def read_func(raw: bytes) -> (Func, int):
-    t, l1 = read_vector(raw, decoder=read_locals)
-    e, l2 = read_expr(raw[l1:])
-    print(f"Read function with locals {t} and instructions {len(e.instructions)}")
-    return Func(t, e), l1 + l2
-
-
-def read_locals(raw: bytes) -> (Locals, int):
-    n, l1 = read_u32(raw)
-    print(f"Locals {n} of type {raw[l1]}")
-    t, l2 = read_valtype(raw[l1:])
-    return Locals(n, t), l1 + l2
-
-
 def parse_element_section(raw: bytes) -> ElementSection:
-    elemsec, _ = read_vector(raw, decoder=read_element)
+    r = io.BytesIO(raw)
+    elemsec = read_vector(r, decoder=read_element)
     return ElementSection(elemsec)
 
 
-def read_element(raw: bytes) -> (Elem, int):
-    x, l1 = read_u32(raw)
-    x = TableIdx(x)
-    e, l2 = read_expr(raw[l1:])
-    y, l3 = read_vector(raw[l1 + l2 :], decoder=read_u32)
-    y = [FuncIdx(z) for z in y]
-    return Elem(x, e, y), l1 + l2 + l3
-
-
 def parse_export_section(raw: bytes) -> ExportSection:
-    exports, _ = read_vector(raw, decoder=read_export)
+    r = io.BytesIO(raw)
+    exports = read_vector(r, decoder=read_export)
     return ExportSection(exports)
 
 
-def read_export(raw: bytes) -> (Export, int):
-    nm, l1 = read_name(raw)
-    d, l2 = read_exportdesc(raw[l1:])
-    return Export(nm, d), l1 + l2
-
-
-def read_exportdesc(raw: bytes) -> (any, int):
-    assert raw[0] <= 3
-    x, l = read_u32(raw[1:])
-    if raw[0] == 0:
-        return FuncIdx(x), l + 1
-    elif raw[0] == 0:
-        return TableIdx(x), l + 1
-    elif raw[0] == 0:
-        return MemIdx(x), l + 1
-    else:
-        return GlobalIdx(x), l + 1
-
-
 def parse_global_section(raw: bytes) -> GlobalSection:
-    globals, _ = read_vector(raw, decoder=read_global)
+    r = io.BytesIO(raw)
+    globals = read_vector(r, decoder=read_global)
     return GlobalSection(globals)
 
 
-def read_global(raw: bytes) -> (Global, int):
-    gt, l1 = read_globaltype(raw)
-    e, l2 = read_expr(raw[l1:])
-    return Global(gt, e), l1 + l2
-
-
-def read_expr(raw: bytes, term=0xB) -> (Expr, int):
-    instructions = []
-    l = 0
-    while raw[l] != term:
-        instr, l2 = read_instruction(raw[l:])
-        l += l2
-        instructions.append(instr)
-    return Expr(instructions), l + 1
-
-
-def read_instruction(raw: bytes) -> (bytes, int):
-    op = raw[0]
-    if op >= 0x45:
-        return raw[:1], 1
-    elif op == 0x00 or op == 0x01 or op == 0x0F:
-        return raw[:1], 1
-    elif op == 0x1A or op == 0x1B:
-        return raw[:1], 1
-    elif op == 0x3F or op == 0x40:
-        return raw[:2], 2
-    elif 0x02 <= op <= 0x04:
-        _, l = read_expr(raw[2:])
-        return raw[: 2 + l], 2 + l
-    elif op == 0xC or op == 0xD or op == 0x10 or (0x20 <= op <= 0x24):
-        _, l = read_u32(raw[1:])
-        return raw[: l + 1], l + 1
-    elif op == 0x0E:
-        _, l = read_vector(raw[1:], decoder=read_u32)
-        _, l2 = read_u32(raw[1 + l :])
-        return raw[: l + l2 + 1], l + l2 + 1
-    elif op == 0x11:
-        _, l = read_u32(raw[1:])
-        return raw[: l + 2], l + 2
-    elif 0x28 <= op <= 0x3E:
-        _, l1 = read_u32(raw[1:])
-        _, l2 = read_u32(raw[1 + l1 :])
-        return raw[: 1 + l1 + l2], 1 + l1 + l2
-    elif op == 0x41:
-        _, l = read_i32(raw[1:])
-        return raw[: 1 + l], 1 + l
-    elif op == 0x42:
-        _, l = read_i64(raw[1:])
-        return raw[: 1 + l], 1 + l
-    elif op == 0x43:
-        _, l = read_f32(raw[1:])
-        return raw[: 1 + l], 1 + l
-    elif op == 0x44:
-        _, l = read_f64(raw[1:])
-        return raw[: 1 + l], 1 + l
-    assert False
-
-
 def parse_memory_section(raw: bytes) -> MemorySection:
-    memories, _ = read_vector(raw, decoder=read_mem)
+    r = io.BytesIO(raw)
+    memories = read_vector(r, decoder=read_mem)
     return MemorySection(memories)
 
 
-def read_mem(raw: bytes) -> (MemType, int):
-    return read_memtype(raw)
-
-
 def parse_table_section(raw: bytes) -> TableSection:
-    tables, _ = read_vector(raw, decoder=read_table)
+    r = io.BytesIO(raw)
+    tables = read_vector(r, decoder=read_table)
     return TableSection(tables)
 
 
-def read_table(raw: bytes) -> (TableType, int):
-    return read_tabletype(raw)
-
-
 def parse_function_section(raw: bytes) -> FunctionSection:
-    funcs, _ = read_vector(raw, decoder=read_typeidx)
+    r = io.BytesIO(raw)
+    funcs = read_vector(r, decoder=read_typeidx)
     return FunctionSection(funcs)
 
 
 def parse_import_section(raw: bytes) -> ImportSection:
-    imports = read_vector(raw, decoder=read_import)
+    r = io.BytesIO(raw)
+    imports = read_vector(r, decoder=read_import)
     return ImportSection(imports)
 
 
-def read_import(raw: bytes) -> (list[Import], int):
-    mod, l1 = read_name(raw)
-    nm, l2 = read_name(raw[l1:])
-    d, l3 = read_importdesc(raw[l1 + l2 :])
-    return Import(mod, nm, d), l1 + l2 + l3
+def read_element(raw: BinaryIO) -> Elem:
+    x = read_u32(raw)
+    x = TableIdx(x)
+    e = read_expr(raw)
+    y = read_vector(raw, decoder=read_u32)
+    y = [FuncIdx(z) for z in y]
+    return Elem(x, e, y)
 
 
-def read_importdesc(raw: bytes) -> (any, int):
-    assert raw[0] <= 3
-    if raw[0] == 0:
-        typeidx, length = read_typeidx(raw[1:])
-        return typeidx, length + 1
-    elif raw[1] == 1:
-        tt, length = read_tabletype(raw[1:])
-        return tt, length + 1
-    elif raw[1] == 2:
-        memtype, length = read_memtype(raw[1:])
-        return memtype, length + 1
+def read_export(raw: BinaryIO) -> Export:
+    nm = read_name(raw)
+    d = read_exportdesc(raw)
+    return Export(nm, d)
+
+
+def read_exportdesc(raw: BinaryIO) -> any:
+    t = raw.read(1)[0]
+    assert t <= 3
+    x = read_u32(raw)
+    if t == 0:
+        return FuncIdx(x)
+    elif t == 1:
+        return TableIdx(x)
+    elif t == 2:
+        return MemIdx(x)
     else:
-        globaltype, length = read_globaltype(raw[1:])
-        return globaltype, length + 1
+        return GlobalIdx(x)
 
 
-def read_globaltype(raw: bytes) -> (GlobalType, int):
-    t, l1 = read_valtype(raw)
-    m = raw[l1] == 1
-    return GlobalType(t, m), l1 + 1
+def read_global(raw: BinaryIO) -> Global:
+    gt = read_globaltype(raw)
+    e = read_expr(raw)
+    return Global(gt, e)
 
 
-def read_memtype(raw: bytes) -> (MemType, int):
-    limits, length = read_limits(raw)
-    return MemType(limits), length
+def peek(r: BinaryIO) -> int:
+    c = r.tell()
+    p = r.read(1)[0]
+    r.seek(c, 0)
+    return p
 
 
-def read_typeidx(raw: bytes) -> (TypeIdx, int):
-    x, length = read_u32(raw)
-    return TypeIdx(x), length
+def read_expr(raw: BinaryIO, term=0xB) -> Expr:
+    instructions = []
+    p = peek(raw)
+    while p != term:
+        instr = read_instruction(raw)
+        instructions.append(instr)
+        p = peek(raw)
+    assert raw.read(1)[0] == term
+    instructions.append(term)
+    return Expr(instructions)
 
 
-def read_tabletype(raw: bytes) -> (TableType, int):
-    et, l1 = read_elemtype(raw)
-    lim, l2 = read_limits(raw[l1:])
-    return TableType(et, lim), l1 + l2
+def read_data(raw: BinaryIO) -> Data:
+    x = read_u32(raw)
+    x = MemIdx(x)
+    e = read_expr(raw)
+    b = read_vector_bytes(raw)
+    return Data(x, e, b)
 
 
-def read_limits(raw: bytes) -> (Limits, int):
-    assert raw[0] <= 1
-    n, l1 = read_u32(raw[1:])
-    if raw[0] == 0:
+def read_code(raw: BinaryIO) -> Code:
+    size = read_u32(raw)
+    c = raw.tell()
+    code = read_func(raw)
+    d = raw.tell()
+    assert size == d - c
+    return Code(size, code)
+
+
+def read_func(raw: BinaryIO) -> Func:
+    t = read_vector(raw, decoder=read_locals)
+    e = read_expr(raw)
+    # print(f"Read function with locals {t} and instructions {len(e.instructions)}")
+    return Func(t, e)
+
+
+def read_locals(raw: BinaryIO) -> Locals:
+    n = read_u32(raw)
+    t = read_valtype(raw)
+    return Locals(n, t)
+
+
+def read_instruction(raw: BinaryIO) -> bytes:
+    op = raw.read(1)[0]
+    bop = bytes((op,))
+
+    if op == 0xFC:
+        subop = read_u32(raw)
+        if subop <= 7:
+            pass
+        elif subop == 12 or subop == 14:
+            read_u32(raw)
+            read_u32(raw)
+        elif subop == 9 or subop == 13 or 15 <= subop <= 17:
+            read_u32(raw)
+        elif subop == 8:
+            read_u32(raw)
+            assert raw.read(1)[0] == 0
+        elif subop == 10:
+            raw.read(2)
+        elif subop == 11:
+            assert raw.read(1)[0] == 0
+        else:
+            print(f"Unknown subop for 0xfc: {subop}")
+            assert False
+
+        return op
+
+    if op == 0xB or op == 0x05:
+        return bop
+    elif 0x45 <= op <= 0xBF:
+        return bop
+    elif op == 0x00 or op == 0x01 or op == 0x0F:
+        return bop
+    elif op == 0x1A or op == 0x1B:
+        return bop
+    elif op == 0x3F or op == 0x40:
+        raw.read(1)
+        return bop
+    elif 0x02 <= op <= 0x04:
+        x = raw.read(1)[0]
+        assert x == 0x40 or 0x7C <= x <= 0x7F
+        read_expr(raw)
+        return bop
+    elif op == 0xC or op == 0xD or op == 0x10 or (0x20 <= op <= 0x24):
+        read_u32(raw)
+        return bop
+    elif op == 0x0E:
+        read_vector(raw, decoder=read_u32)
+        read_u32(raw)
+        return bop
+    elif op == 0x11:
+        read_u32(raw)
+        raw.read(1)
+        return bop
+    elif 0x28 <= op <= 0x3E:
+        read_u32(raw)
+        read_u32(raw)
+        return bop
+    elif op == 0x41:
+        read_i32(raw)
+        return bop
+    elif op == 0x42:
+        read_i64(raw)
+        return bop
+    elif op == 0x43:
+        read_f32(raw)
+        return bop
+    elif op == 0x44:
+        read_f64(raw)
+        return bop
+    print(f"op = {op:x}")
+    assert False
+
+
+def read_mem(raw: BinaryIO) -> MemType:
+    return read_memtype(raw)
+
+
+def read_table(raw: BinaryIO) -> TableType:
+    return read_tabletype(raw)
+
+
+def read_import(raw: BinaryIO) -> list[Import]:
+    mod = read_name(raw)
+    nm = read_name(raw)
+    d = read_importdesc(raw)
+    return Import(mod, nm, d)
+
+
+def read_importdesc(raw: BinaryIO) -> any:
+    t = raw.read(1)[0]
+    assert t <= 3
+    if t == 0:
+        return read_typeidx(raw)
+    elif t == 1:
+        return read_tabletype(raw)
+    elif t == 2:
+        return read_memtype(raw)
+    else:
+        return read_globaltype(raw)
+
+
+def read_globaltype(raw: BinaryIO) -> GlobalType:
+    t = read_valtype(raw)
+    m = raw.read(1)[0] == 1
+    return GlobalType(t, m)
+
+
+def read_memtype(raw: BinaryIO) -> MemType:
+    limits = read_limits(raw)
+    return MemType(limits)
+
+
+def read_typeidx(raw: BinaryIO) -> TypeIdx:
+    x = read_u32(raw)
+    return TypeIdx(x)
+
+
+def read_tabletype(raw: BinaryIO) -> TableType:
+    et = read_elemtype(raw)
+    lim = read_limits(raw)
+    return TableType(et, lim)
+
+
+def read_limits(raw: BinaryIO) -> Limits:
+    t = raw.read(1)[0]
+    assert t <= 1
+    n = read_u32(raw)
+    if t == 0:
         m = None
-        l2 = 0
     else:
-        m, l2 = read_u32(raw[1 + l1 :])
-    return Limits(n, m), 1 + l1 + l2
+        m = read_u32(raw)
+    return Limits(n, m)
 
 
-def read_elemtype(raw: bytes) -> (FuncRef, int):
-    assert raw[0] == 0x70
+def read_elemtype(raw: BinaryIO) -> FuncRef:
+    assert raw.read(1)[0] == 0x70
     return FuncRef, 1
 
 
 def parse_type_section(raw: bytes) -> TypeSection:
-    function_types = read_vector(raw, decoder=read_function_type)
+    r = io.BytesIO(raw)
+    function_types = read_vector(r, decoder=read_function_type)
     return TypeSection(function_types)
 
 
-def read_function_type(raw: bytes) -> (FunctionType, int):
-    assert raw[0] == 0x60
-    parameter_types, len = read_vector(raw[1:], decoder=read_valtype)
-    result_types, len2 = read_vector(raw[1 + len :], decoder=read_valtype)
-    return FunctionType(parameter_types, result_types), 1 + len + len2
+def read_function_type(raw: BinaryIO) -> FunctionType:
+    assert raw.read(1)[0] == 0x60
+    parameter_types = read_vector(raw, decoder=read_valtype)
+    result_types = read_vector(raw, decoder=read_valtype)
+    return FunctionType(parameter_types, result_types)
 
 
-def read_valtype(raw: bytes) -> (ValType, int):
-    return ValType(raw[0]), 1
+def read_valtype(raw: BinaryIO) -> ValType:
+    return ValType(raw.read(1)[0])
 
 
 def parse_custom_section(raw: bytes) -> CustomSection:
-    name, length = read_name(raw)
-    data = raw[length:]
+    r = io.BytesIO(raw)
+    name = read_name(r)
+    data = r.read()
     return CustomSection(name, data)
 
 
-def read_name(raw: bytes) -> (str, int):
-    name, length = read_vector_bytes(raw)
+def read_name(raw: BinaryIO) -> str:
+    name = read_vector_bytes(raw)
     name = name.decode("utf8")
-    return name, length
+    return name
 
 
-def read_vector(
-    raw: bytes, decoder: Callable[[bytes], Tuple[any, int]]
-) -> (list[any], int):
-    vlen, length = read_u32(raw)
+def read_vector(raw: BinaryIO, decoder: Callable[[BinaryIO], any]) -> list[any]:
+    vlen = read_u32(raw)
     v = []
     for _ in range(vlen):
-        x, l = decoder(raw[length:])
-        length += l
+        x = decoder(raw)
         v.append(x)
-    return v, length
+    return v
 
 
-def read_vector_bytes(raw: bytes) -> (bytes, int):
-    vlen, length = read_u32(raw)
-    return raw[length : length + vlen], length + vlen
+def read_vector_bytes(raw: BinaryIO) -> bytes:
+    vlen = read_u32(raw)
+    return raw.read(vlen)
 
 
-def read_u32(f: bytes) -> (int, int):
+def read_u32_bytes(f: bytes) -> (int, int):
     l = 0
-    num = f[0] & 0x7F
-    while f[l] & 0x80:
+    b = f[0]
+    num = b & 0x7F
+    while b & 0x80:
         l += 1
-        num |= (f[l] & 0x7F) << (7 * l)
+        b = f[l]
+        num |= (b & 0x7F) << (7 * l)
     return num, l + 1
 
 
-def read_i32(f: bytes) -> (int, int):
+def read_u32(f: BinaryIO) -> int:
     l = 0
-    num = f[0] & 0x7F
-    while f[l] & 0x80:
+    b = f.read(1)[0]
+    num = b & 0x7F
+    while b & 0x80:
         l += 1
-        num |= (f[l] & 0x7F) << (7 * l)
-    if f[l] & 0x40:
+        b = f.read(1)[0]
+        num |= (b & 0x7F) << (7 * l)
+    return num
+
+
+def read_i32(f: BinaryIO) -> int:
+    l = 0
+    b = f.read(1)[0]
+    num = b & 0x7F
+    while b & 0x80:
+        l += 1
+        b = f.read(1)[0]
+        num |= (b & 0x7F) << (7 * l)
+    if b & 0x40:
         num = (-num) & ((1 << (7 * (l + 1) - 1)) - 1)
         num = -num
-    return num, l + 1
+    return num
 
 
-def read_i64(f: bytes) -> (int, int):
+def read_i64(f: BinaryIO) -> int:
     return read_i32(f)
 
 
-def read_f32(f: bytes) -> (float, int):
-    return struct.unpack("<f", f[:4])[0], 4
+def read_f32(f: BinaryIO) -> float:
+    return struct.unpack("<f", f.read(4))[0]
 
 
-def read_f64(f: bytes) -> (float, int):
-    return struct.unpack("<d", f[:8])[0], 8
+def read_f64(f: BinaryIO) -> float:
+    return struct.unpack("<d", f.read(8))[0]
 
 
 with open(sys.argv[1], "rb") as fin:
